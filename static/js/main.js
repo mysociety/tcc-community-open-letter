54937fbae735939e942069472409bdcf0983b46d
Zarino Zappia <mail@zarino.co.uk>
2024-07-21 16:32:55 +0100

    TCC branding, homepage content, searchable list

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,21 @@
+var standardiseText = function(string){
+    return string.toLowerCase().replace(/[^\p{L}\p{N}]/gu, '');
+}
+
 $(function(){
-    console.log('jQuery ready')
-    console.log('_.now()', _.now())
-    console.log('bootstrap.Modal', bootstrap.Modal);
-})
+    $('#search').on('keyup', function(){
+        var needle = standardiseText( $(this).val() );
+        if ( needle !== '' ) {
+            $('#signatories tbody tr').each(function(){
+                var haystack = standardiseText( $(this).text() );
+                if ( haystack.indexOf(needle) > -1 ) {
+                    $(this).removeClass('d-none');
+                } else {
+                    $(this).addClass('d-none');
+                }
+            });
+        } else {
+            $('#signatories tbody tr').removeClass('d-none');
+        }
+    });
+});
